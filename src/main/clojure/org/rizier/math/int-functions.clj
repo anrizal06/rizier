1d41410be82fe62298631305845f77f89f83b786
unknown <rizal@.(none)>
2011-01-06 04:02:48 +0100

    Finalization of saddleback search

--- src/main/clojure/org/rizier/math/int-functions.clj
+++ src/main/clojure/org/rizier/math/int-functions.clj
@@ -30,10 +30,9 @@
                (digits-of (quot x 10) (cons (rem x 10) acc))))]
         (digits-of x [])))
         
-; The design of the invert function that given a function f that takes
-; a pair of natural numbers and a natural number z, returns a list of 
-; all pairs (x,y) satisfying f(x,y)= z. The function f is strictly
-; increasing on each argument.
+; Given a function f that takes a pair of natural numbers and a natural 
+; number z, returns a list of all pairs (x,y) satisfying f(x,y)= z. 
+; The function f is strictly increasing on each argument.
 ; The problem is known as saddleback search problem.
 ; The solution here is explained in (Bird, 2010)
 (defn invert-simple 
@@ -45,10 +44,10 @@
      	      (cond (or (> u z) (< v 0)) nil
      	   	   (< zz z) (find-invert (inc u) v f z)
      	   	   (= zz z) (cons [u v] (find-invert (inc u) (dec v) f z))
-     	   	   	   (> zz z) (find-invert u (dec v) f z))))]
+     	   	   (> zz z) (find-invert u (dec v) f z))))]
      (find-invert 0 z f z)))
      
-; binary search to find the limit of the solution space.
+; Binary search to find the limit of the solution space.
 ; g is the function. it must be strictly increasing on each argument.
 ; left is the lower bound, right is upper bound
 ; z is the number being searched.
@@ -60,12 +59,15 @@
                 (> midval z) (bsearch g left (dec mid) z)))))
 
 ; Optimized invert.		
+; Instead of starting from (0,z), the solution deploys first binary 
+; search to limit the solution space to (0, m) and (n, 0) where 
+; m <= z and n <= z.
 (defn invert 
     "The optimized invert-simple algorithm. The solution space limit 
-     is no longer (0, z) to (z, z), but from (0,m) to (r,s). "
+     is no longer (0, z) to (z, z), but from (0,m) to (n,0). "
      [f z]
     (let [find-invert
-    	  (fn find-invert [ [u v] [r s] f z]
+    	  (fn find-invert [ [u v] [r s] f z ]
      	     (let [zz (f u v)]
      	      (cond (or (> u r) (< v s)) nil
      	   	   (< zz z) (find-invert [(inc u) v] [r s] f z)
--- src/main/scala/org/rizier/math/IntFunctions.scala
+++ src/main/scala/org/rizier/math/IntFunctions.scala
@@ -28,6 +28,62 @@ object IntFunctions {
     digits(x, Nil)
  }
  
+ /** Given a function f that takes a pair of natural numbers and a natural
+ *   number of z, returns a lit of all pairs (x,y) satisfying f(x,y)= z.
+ *   The function f is strictly increasing on each argument.
+ *   The problem is known as saddleback search problem and the solution
+ *   is explained in (Bird, 2010).
+ */
+ def invertSimple(f: (Int, Int) => Int, z: Int) = {
+     import scala.annotation.tailrec
+     @tailrec def findInvert(u: Int, v: Int, acc: List[(Int, Int)]):  List[ (Int, Int) ] = {
+           if ( u > z || v < 0) acc
+           else {
+              val zz = f(u,v)
+              if (zz < z) findInvert(u + 1, v, acc)
+              else if (zz == z) findInvert(u + 1, v - 1, (u,v) :: acc)
+              else findInvert(u, v - 1, acc)
+           }
+     }
+     findInvert(0, z, Nil)
+ }
+ 
+ def binarySearch( f:(Int) => Int, left: Int, right: Int, z: Int): Int = {
+    val mid = (left + right) / 2
+    val midval = f(mid)
+    if ( left > right) left
+    else if (midval <= z) binarySearch(f, mid + 1, right, z)
+    else binarySearch(f, 0, mid - 1, z)
+    
+     
+ }
+ 
+ /** The optimized version of invert simple where the search starts
+ *   and ends not at (0,z) to (z, 0), but from (0,m) to (n, 0). The
+ *   optimization is done by deploying binary search to discover 
+ *   m and n.
+ */
+ def invert(f:(Int, Int) => Int, z: Int) = {
+     
+     def findInvert( u: Int, v:Int, r:Int, s: Int, acc: List[ (Int, Int)]):
+        List[ (Int, Int) ]= {
+        if (u > r || v < s) acc
+        else {
+
+            val zz = f(u,v);
+            if (zz < z) findInvert(u + 1, v, r, s, acc)
+            else if (zz == z) findInvert(u + 1, v - 1, r, s, (u,v) :: acc)
+            else findInvert(u, v - 1, r, s, acc)
+        }
+     }
+     findInvert(0,
+        binarySearch(f(0, _), 0, z, z), 
+        binarySearch(f(_,0), 0, z, z), 
+        0, 
+        Nil)  
+ }
 }
+
+
 
 
